d480d06a8f36814206040b80670e8cfa9d034141
xthegoodguy <xashinx@gmail.com>
2018-01-26 15:16:52 -0600

    added in code for MVC, added in routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,27 @@
+require 'wikipedia'
 class Api::V1::UsersController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    user = User.new(
+                    name: params[:name],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation]
+                    )
+    if user.save
+      session[:user_id] = user.id 
+      flash[:success] = "Successfully created account"
+      redirect_to '/api/v1/'
+    else
+      flash[:warning] = "Invalid email or password!"
+      redirect_to '/api/v1/signup'
+    end
+  end
+
+  def show
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/' => 'articles#home'
+      get '/home' => 'articles#home'
+      get '/articles' => 'articles#index'
+      get '/articles' => 'articles#new'
+      post '/articles' => 'articles#create'
+      get '/articles/:article_title' => 'articles#lookup'
+      get '/articles/user_article/:id' => 'articles#show'
+      delete '/articles/:id' => 'articles#destroy'
+      get '/contact' => 'articles#contact'
+
+      get '/highlights' => 'highlights#index'
+      post '/highlights' => 'highlights#create'
+      post '/highlights/extension/:article_title' => 'highlights#api_find_create'
+      patch '/highlights/:id' => 'highlights#update'
+      delete '/highlights/:id' => 'highlights#destroy'
+
+      get '/signup' => 'users#new'
+      post '/users' => 'users#create'
+      get '/users/:id' => 'users#show'
+
+      get '/login' => 'sessions#new'
+      post '/login' => 'sessions#create'
+      get '/logout' => 'sessions#destroy'
+    end
+  end
 end
